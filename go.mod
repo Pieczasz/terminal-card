72c0092975e8296bce5d4953b13f376f6c9d1383
Pieczasz <bartekp854@gmail.com>
2026-04-20 23:36:15 +0200

    feat: move "manager" logic to matchmaking, use with pattern for options, create RWMutex lobby manager, and common methods to work with lobbies

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/charmbracelet/lipgloss v1.1.1-0.20250404203927-76690c660834
 	github.com/charmbracelet/ssh v0.0.0-20250826160808-ebfa259c7309
 	github.com/charmbracelet/wish v1.4.7
+	github.com/stretchr/testify v1.11.1
 	golang.org/x/crypto v0.49.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
@@ -28,6 +29,7 @@ require (
 	github.com/clipperhouse/displaywidth v0.11.0 // indirect
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
 	github.com/creack/pty v1.1.24 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/go-logfmt/logfmt v0.6.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -36,6 +38,7 @@ require (
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
@@ -43,10 +46,13 @@ require (
 	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/exp v0.0.0-20260312153236-7ab1446f8b90 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.42.0 // indirect
 	golang.org/x/text v0.35.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/lobby/matchmaking.go
+++ internal/lobby/matchmaking.go
@@ -1,1 +1,200 @@
+// Package lobby contains implementation of game lobby - it is responsible for
+// random game code generations, players game invitations, starting the game,
+// selecting card game and options.
 package lobby
+
+import (
+	"client/internal/db"
+	"crypto/rand"
+	"errors"
+	"log"
+	"math/big"
+	"slices"
+	"sync"
+)
+
+// Lobby contains all information related to a lobby.
+type Lobby struct {
+	mu        sync.RWMutex
+	leader    *db.User
+	guests    []*db.User
+	options   *lobbyOptions
+	lobbyCode string
+}
+
+// Manager handles the lifecycle and retrieval of all active lobbies.
+type Manager struct {
+	mu      sync.RWMutex
+	lobbies map[string]*Lobby
+}
+
+// Option defines a functional option for configuring a Lobby.
+type Option func(*lobbyOptions)
+
+// lobbyOptions holds the configuration for a lobby.
+type lobbyOptions struct {
+	cardGame   *db.Game
+	maxPlayers int
+	isPrivate  bool
+}
+
+// WithCardGame sets the card game for the lobby.
+func WithCardGame(game *db.Game) Option {
+	return func(o *lobbyOptions) {
+		o.cardGame = game
+	}
+}
+
+// WithMaxPlayers sets the maximum number of players for the lobby.
+func WithMaxPlayers(max int) Option {
+	return func(o *lobbyOptions) {
+		o.maxPlayers = max
+	}
+}
+
+// WithPrivate sets the privacy status of the lobby.
+func WithPrivate(isPrivate bool) Option {
+	return func(o *lobbyOptions) {
+		o.isPrivate = isPrivate
+	}
+}
+
+// NewManager creates and returns a new Lobby Manager.
+func NewManager() *Manager {
+	return &Manager{
+		lobbies: make(map[string]*Lobby),
+	}
+}
+
+// NewLobby creates a new lobby and registers it in the manager.
+func (m *Manager) NewLobby(leader *db.User, opts ...Option) (*Lobby, error) {
+	options := &lobbyOptions{
+		maxPlayers: 4,
+		isPrivate:  false,
+	}
+
+	for _, opt := range opts {
+		opt(options)
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	// Generate a unique lobby code with collision handling
+	var code string
+	maxRetries := 10
+	for range maxRetries {
+		code = generateLobbyCode(6)
+		if _, exists := m.lobbies[code]; !exists {
+			break
+		}
+		code = ""
+	}
+
+	if code == "" {
+		log.Print("failed to generate unique lobby code after maximum retries, how tf we ended up here")
+		return nil, errors.New("unexpected error happen, please try again")
+	}
+
+	lobby := &Lobby{
+		leader:    leader,
+		guests:    make([]*db.User, 0, options.maxPlayers-1),
+		options:   options,
+		lobbyCode: code,
+	}
+
+	m.lobbies[code] = lobby
+	return lobby, nil
+}
+
+// FindLobbyByCode retrieves a lobby by its lobby code. Throws an error if the lobby
+// is not found.
+func (m *Manager) FindLobbyByCode(code string) (*Lobby, error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	lobby, exists := m.lobbies[code]
+	if !exists {
+		return nil, errors.New("lobby not found")
+	}
+
+	return lobby, nil
+}
+
+// JoinLobbyByCode attempts to add a user to an existing lobby by code.
+func (m *Manager) JoinLobbyByCode(code string, player *db.User) error {
+	lobby, err := m.FindLobbyByCode(code)
+	if err != nil {
+		return err
+	}
+
+	return lobby.addGuest(player)
+}
+
+// RemovePlayer removes a player from the lobby.
+// It returns true if the lobby is now empty and should be deleted.
+func (l *Lobby) RemovePlayer(player *db.User) bool {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if l.leader == player {
+		if len(l.guests) > 0 {
+			l.leader = l.guests[0]
+			l.guests = l.guests[1:]
+		} else {
+			return true
+		}
+	} else {
+		l.guests = slices.DeleteFunc(l.guests, func(p *db.User) bool {
+			return p == player
+		})
+	}
+
+	return false
+}
+
+// RemoveLobby deletes a lobby from the manager.
+func (m *Manager) RemoveLobby(code string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	delete(m.lobbies, code)
+}
+
+// TODO: is user comparision by reference to db.User safe?
+func (l *Lobby) addGuest(player *db.User) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	if 1+len(l.guests) >= l.options.maxPlayers {
+		return errors.New("this lobby is full")
+	}
+
+	if l.leader == player {
+		return errors.New("player is already the leader of this lobby")
+	}
+	if slices.Contains(l.guests, player) {
+		return errors.New("player is already in this lobby")
+	}
+
+	l.guests = append(l.guests, player)
+	return nil
+}
+
+// Code returns the lobby's code
+func (l *Lobby) Code() string {
+	// I guess we dont need locks here since code is immutable.
+	return l.lobbyCode
+}
+
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+// generateLobbyCode creates a random string of the specified length.
+func generateLobbyCode(length int) string {
+	code := make([]byte, length)
+	charsetLen := big.NewInt(int64(len(charset)))
+	for i := range code {
+		n, _ := rand.Int(rand.Reader, charsetLen)
+		code[i] = charset[n.Int64()]
+	}
+	return string(code)
+}
